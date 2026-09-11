4ac74ed5318ec284aa87a3a062d24e23496bd9ac
surafel5509-del <surafel5509@gmail.com>
2026-09-11 08:38:15 +0000

    fix: use Vercel Remix server handler

---
+++ app/entry.server.tsx
@@ -0,0 +1,32 @@
+import { handleRequest as vercelHandleRequest } from '@vercel/remix';
+import type { EntryContext } from '@vercel/remix';
+import { RemixServer } from '@remix-run/react';
+import { themeStore } from '~/lib/stores/theme';
+import { renderToString } from 'react-dom/server';
+import { Head } from './root';
+import { renderHeadToString } from 'remix-island';
+
+export default function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext,
+) {
+  const head = renderHeadToString({ request, remixContext, Head });
+  const remixServer = <RemixServer context={remixContext} url={request.url} />;
+
+  const htmlStart = `<!DOCTYPE html><html lang="en" data-theme="${themeStore.value}"><head>${head}</head><body><div id="root" class="w-full h-full">`;
+  const htmlEnd = '</div></body></html>';
+
+  responseHeaders.set('Cross-Origin-Embedder-Policy', 'require-corp');
+  responseHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
+
+  return vercelHandleRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixServer,
+    htmlStart,
+    htmlEnd,
+  );
+}
